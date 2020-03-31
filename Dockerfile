529465cd5dabb9050bba8862da2c2f20e6354067
cmlee <bal.dongdongdong@gmail.com>
2020-03-31 22:59:01 +0900

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,26 @@
-FROM ubuntu:16.04
+FROM node:10-slim
 
-RUN apt-get update
-RUN apt-get install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+# Install latest chrome dev package and fonts to support major charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
+# Note: this installs the necessary libs to make the bundled version of Chromium that Puppeteer
+# installs, work.
+RUN apt-get update \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
 
-RUN apt-get update -yq \
-    && apt-get install curl gnupg -yq \
-    && curl -sL https://deb.nodesource.com/setup_8.x | bash \
-    && apt-get install nodejs -yq
-
-RUN mkdir -p /app
 WORKDIR /app
 
-COPY build/ ./build
 COPY package*.json ./
+
+RUN npm ci --only=production
+RUN npm install pm2 -g
+
+COPY build/ ./build
 COPY .env ./
 COPY ecosystem.config.js ./
 
-#RUN ["npm", "install"]
-#CMD bash && npm run server:reload
-#CMD ["npm", "run", "server:reload"]
+CMD ["npm", "run", "server:reload"]
--- src/lib/TelegramChatBot.js
+++ src/lib/TelegramChatBot.js
@@ -2,49 +2,51 @@ process.env.NTBA_FIX_319 = 1;
 
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_KEY;
-const bot = new TelegramBot(token, { polling: true });
 
-function TelegramChatBot(chatId) {
-  this.chatId = chatId;
+class TelegramChatBot {
+  constructor(chatId) {
+    this.chatId = chatId;
+    this.bot = new TelegramBot(token, { polling: true });
 
-  bot.onText(/\/chatId/, (msg) => {
-    const chatId = msg.chat.id;
-    if (this.chatId === chatId) bot.sendMessage(chatId, `this chat id is '${chatId}'`);
-  });
+    this.bot.onText(/\/chatId/, (msg) => {
+      const chatId = msg.chat.id;
+      if (this.chatId === chatId) this.bot.sendMessage(chatId, `this chat id is '${chatId}'`);
+    });
 
-  bot.onText(/\/echo (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const resp = match[1];
+    this.bot.onText(/\/echo (.+)/, (msg, match) => {
+      const chatId = msg.chat.id;
+      const resp = match[1];
 
-    bot.sendMessage(chatId, resp);
-  });
+      this.bot.sendMessage(chatId, resp);
+    });
 
-  bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-  });
+    this.bot.on('message', (msg) => {
+      const chatId = msg.chat.id;
+    });
+  }
 
-  this.addOnText = (match, callback) => {
-    bot.onText(match, callback);
+  addOnText = (match, callback) => {
+    this.bot.onText(match, callback);
   };
 
-  this.send = (msg) => {
-    bot.sendMessage(this.chatId, msg, { parse_mode: 'HTML' });
+  send = (msg) => {
+    this.bot.sendMessage(this.chatId, msg, { parse_mode: 'HTML' });
   };
 
-  this.sendPhoto = (url) => {
-    bot.sendPhoto(this.chatId, url);
+  sendPhoto = (url) => {
+    this.bot.sendPhoto(this.chatId, url);
   };
 
-  this.getToken = () => {
+  getToken = () => {
     return token;
   };
 
-  this.getChatId = () => {
-    return chatId;
+  getChatId = () => {
+    return this.chatId;
   };
 
-  this.setChatId = (cId) => {
-    chatId = cId;
+  setChatId = (cId) => {
+    this.chatId = cId;
   };
 }
 
